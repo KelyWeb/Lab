5a9f459482f026c378c695502ccbf08bc7eea2eb
KelyWeb <48602536+KelyWeb@users.noreply.github.com>
2020-09-21 18:46:25 +0300

    Create maxNumber.c

---
+++ maxNumber.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int max2(int, int);
+
+int main(){
+    int a, b, c;
+    printf("Введите число a & b: \n");
+    scanf("%d%d%d", &a, &b, &c);
+    printf("Самое большое число из 3: ");
+    printf("%d\n", max2(max2(a, b), c));
+}
+
+
+int max2(int a, int b){
+    if (a >= b){
+        return a;
+    }
+    else{
+        return b;
+    }
+}
