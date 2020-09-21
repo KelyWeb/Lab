30a639d373923cb16ab3ae060b88cd65c6cfa12c
KelyWeb <48602536+KelyWeb@users.noreply.github.com>
2020-09-21 18:27:46 +0300

    Create lab1.c

---
+++ lab1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<locale.h>
+#include<string.h>
+
+int main(){
+    setlocale(LC_ALL, "Russian");
+    char word[60];
+    printf("Введите строку: ");
+    scanf("%[^\n]", word);
+    
+    for(int i = 0; i < strlen(word); i++){ //Перебор строки для взятия символа на подсчет.
+        int schetchik = 0;
+        for(int b = 0; b < strlen(word); b++){ //Перебор строки для подсчета конкретного символа.
+            if(word[b] == word[i]){
+                schetchik++;
+            }
+        }
+    printf("Количество буквы %c = %d\n", word[i], schetchik);
+    }
+}
