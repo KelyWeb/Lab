e139a0a4776d965a0664db5dd016106514309b65
KelyWeb <48602536+KelyWeb@users.noreply.github.com>
2020-09-24 18:16:49 +0300

    Update maxNumber.c

--- maxNumber.c
+++ maxNumber.c
@@ -4,7 +4,7 @@ int max2(int, int);
 
 int main(){
     int a, b, c;
-    printf("Введите число a & b: \n");
+    printf("Введите число a & b & c: \n");
     scanf("%d%d%d", &a, &b, &c);
     printf("Самое большое число из 3: ");
     printf("%d\n", max2(max2(a, b), c));
