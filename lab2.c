30aa5d14a5e6828ab514f977bc0b8876e4479529
KelyWeb <48602536+KelyWeb@users.noreply.github.com>
2020-09-30 09:16:34 +0300

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,19 +1,13 @@
 #include<stdio.h>
+#include<locale.h>
 #include<math.h>
 
 
-
-int main(){ 
-point:      
-    int x = 0;
+int main(){
+    setlocale(LC_ALL, "Russian");
     float y;
-    scanf("%d", &x);
-    if (x > 0 && x < 1.5){
-        y = ((cos(x)*cos(x) - sin(x - 1.5) - pow(x, 3)) / (x + 1)) + sqrt((cos(x)) / (pow(x, 2) + 0.1));
-        printf("%f", y);
-    }
-    else{
-        printf("Введите число от 0 до 1.5");
-        goto point;
+    for(float x = 0; x < 1.5; x+=0.1){
+        y = ((cos(x)*cos(x) - sin(x-1.5) - pow(x, 3)) / (x + 1) +sqrt((cos(x)) / pow(x, 2) +0.1));
+        printf("Значение y при x %f = %f", x, y);
     }
 }
