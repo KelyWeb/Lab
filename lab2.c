5df48738ce02bfc800c4b5730776926ae6406877
KelyWeb <48602536+KelyWeb@users.noreply.github.com>
2020-09-30 09:53:26 +0300

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -3,11 +3,11 @@
 #include<math.h>
 
 
-int main(){
+int main() {
     setlocale(LC_ALL, "Russian");
     float y;
-    for(float x = 0; x < 1.5; x+=0.1){
-        y = (((cos(x)*cos(x) - sin(x-1.5) - pow(x, 3)) / (x + 1)) +sqrt((cos(x) / (pow(x, 2) + 0.1)));
-        printf("Значение y при x %f = %f", x, y);
+    for (float x = 0; x < 1.6; x += 0.1) {
+        y = (((cos(x) * cos(x) - sin(x - 1.5) - pow(x, 3)) / (x + 1)) + sqrt((cos(x) / (pow(x, 2) + 0.1))));
+        printf("Значение y при x %.1f = %f\n", x, y);
     }
 }
