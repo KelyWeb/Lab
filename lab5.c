f1d1a5803b1ec84e968d9acd70d0ee86eb659c88
KelyWeb <48602536+KelyWeb@users.noreply.github.com>
2020-10-14 01:29:03 +0300

    Update lab5.c

--- lab5.c
+++ lab5.c
@@ -1,47 +1,57 @@
 #include<stdio.h>
 #include<locale.h>
-#define kl 5
+#include<stdlib.h>
+#define kl 4
 
-int main(){
-    int ms[5][5]; //Наполнение матрицы числами.
-    for(int i = 0; i < kl; i++){
+void main(void){
+    setlocale(LC_ALL, "Russian");
+    int ms[kl][kl];
+
+    //Наполнение матрицы элементами.
+    for (int i = 0; i < kl; i++){
         for (int b = 0; b < kl; b++){
-            int apend;
-            scanf("%d", &apend);
-            ms[i][b] = apend;
+            int append; 
+            scanf("%d", &append);
+            ms[i][b] = append;
+        }
+    }
+
+    int max = ms[0][0];
+    int ravn_max_position_str = -1;
+    int ravn_max_position_stolb = -1;
+    //Перебор элементов диагонали для нахождения максимального элемента.
+    for (int i = 0; i < kl; i++){
+        if(max < ms[i][i]){
+            max = ms[i][i];
         }
     } 
 
-
-    for (int i = 1; i < kl; i++){ //Начало(2)
-        int max = ms[1][1];
-        if (ms[i][i] > max){
-            max = ms[i][i];
-            for (int b = i; b > 0; b--){
-                if (max < ms[i][b]){
-                    max = ms[i][b];
-                }
-                else{
-                    continue;
-                }
+    //Перебор элементов матрицы ниже диагонали для нахождения числа = max.
+    for (int b = 0; b < kl; b++){
+        for (int d = b - 1; d >= 0; d--){
+            if(ms[b][d] == max){
+                ravn_max_position_str = b;
+                ravn_max_position_stolb = d;
             }
         }
-        else{
-            continue;
-        }
-        printf("\n%d ", max);
-    }//Конец(2)
+    }
 
-
-
-    for (int i = 0; i < kl; i++){ //Вывод матрицы для проверки.
-        for (int b = 0; b < kl; b++){
-            printf("%d ", ms[i][b]);
+    //Вывод информации.
+    system("clear");
+    for (int x = 0; x < kl; x++){
+        for (int y = 0; y < kl; y++){
+            printf("[%d] ", ms[x][y]);
         }
         printf("\n");
     }
-    
+    printf("\nМаксимальный элемент главной диагонали: %d\n", max);
 
 
-
+    if (ravn_max_position_stolb == -1 && ravn_max_position_str == -1){
+        printf("Ниже главной диагонали нет числа, равного максимальному на глайной диагонали.");
+    }
+    else{
+        printf("Ниже главной диагонали есть элемент равный максимальному элементу главной диагонали.\n");
+        printf("Схожий элемент: Строка: %d Столбец: %d \n", ravn_max_position_str, ravn_max_position_stolb);
+    }
 }
