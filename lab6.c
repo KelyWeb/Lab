35352194faadc4ed74a2c107af604c681a30d67b
KelyWeb <48602536+KelyWeb@users.noreply.github.com>
2020-11-23 23:26:59 +0300

    Update lab6.c

--- lab6.c
+++ lab6.c
@@ -15,9 +15,9 @@ int main(void){
     printf("Матрица будет состоять из %d строк и %d столбцов\n", s_kl, st_kl);
 
     //Память под массив.
-    ms = (int **)malloc(s_kl*sizeof(int *));
+    ms = (int **)malloc(s_kl * sizeof(int *));
     for(int i = 0; i < s_kl; i++){
-        ms[i] = (int *)malloc(st_kl*sizeof(int));
+        ms[i] = (int *)malloc(st_kl * sizeof(int));
     }
 
 
@@ -30,6 +30,31 @@ int main(void){
     }
     system("clear");
     
+
+    //Нахождение максимального числа в матрице
+    int check_num = ms[0][0];
+    for (int i = 1; i < s_kl; i++){
+        for(int b = 1; b < s_kl; b++){
+            if (check_num < ms[i][b]){
+                check_num = ms[i][b];
+            }
+        }
+    }
+    //printf("num = %d", check_num);
+
+    int check2 = ms[0][0];
+    for(int i = 0; i < 1; i++){
+        for(int b = 0; b < s_kl; b++){
+            if(ms[i][b] > check2){
+                check2 = ms[i][b];
+            }
+            if(ms[b][i] > check2){
+                check2 = ms[b][i];
+            }
+        }
+    }
+    //printf("\nchek2 = %d", check2);
+
 
     //Вывод введенного массива.
     printf("Матрица: \n");
@@ -47,7 +72,7 @@ int main(void){
         int point = 0;
         int * check;
         int position = 0;
-        check = (int *)malloc((i+i)*sizeof(int));
+        check = (int *)malloc((s_kl + s_kl) * sizeof(int));
         for(int b = 0; b < st_kl; b++){
             check[position] = ms[i][b];
             check[position+1] = ms[b][i];
@@ -65,7 +90,7 @@ int main(void){
             int number = check[j];
             int schetchik = 0;
             for(int e = 0; e < s_kl*2; e++){
-                if (check[e] == number){
+                if ((check[e] == number) || (check2 < check_num)){
                     schetchik++;
                     if(schetchik > 2){
                         printf("Матрица не является латинским квадратом.\n");
@@ -87,5 +112,3 @@ int main(void){
 
 
 }
-
-
